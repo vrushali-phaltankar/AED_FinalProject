02b72e529efce43b4f5c686e1930b7fe697d157c
Vrushali Phaltankar <phaltankar.v@northeastern.edu>
2022-12-10 01:14:39 -0500

    Roles

---
+++ src/Business/Roles/FoodStandardAdminRole.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Roles;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import Interface.FoodStandardInspector.FoodCollectionAdmin;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author vrushaliphaltankar
+ */
+public class FoodStandardAdminRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new FoodCollectionAdmin(userProcessContainer,account);
+    }
+    
+    @Override
+    public String toString(){
+        return Role.RoleType.Supervisor.getValue();
+        
+    }
+    
+}
